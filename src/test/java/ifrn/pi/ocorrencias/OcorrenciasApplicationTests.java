37e2ddda0ed4193cf89fc71588e5c718a12122f3
AnaellyN <anaellynobre24@gmail.com>
2022-12-12 11:37:49 -0300

    Arquivos inicias da aplicação

---
+++ src/test/java/ifrn/pi/ocorrencias/OcorrenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package ifrn.pi.ocorrencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcorrenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
