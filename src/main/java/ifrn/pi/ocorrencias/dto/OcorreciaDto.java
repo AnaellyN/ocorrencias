b7f1afe3ccac551d50781fc2f9834e60aa556a3d
Annacelia24 <anaceliadandara8@gmail.com>
2022-12-27 16:15:56 -0300

    Criando Classe Ocorrência Dto

---
+++ src/main/java/ifrn/pi/ocorrencias/dto/OcorreciaDto.java
@@ -0,0 +1,43 @@
+package ifrn.pi.ocorrencias.dto;
+
+public class OcorreciaDto {
+	
+	private String data;
+	private String horario;
+	private String principal_sintoma;
+	private String principal_motivo;
+		
+
+	public  OcorreciaDto(String data, String horario,String principal_sintoma,String principal_motivo) {
+		this.data = data;
+		this.horario = horario;
+		this.principal_sintoma = principal_sintoma;
+		this.principal_motivo = principal_motivo;
+		
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	public String getPrincipal_sintoma() {
+		return principal_sintoma;
+	}
+	public void setPrincipal_sintoma(String principal_sintoma) {
+		this.principal_sintoma = principal_sintoma;
+	}
+	public String getPrincipal_motivo() {
+		return principal_motivo;
+	}
+	public void setPrincipal_motivo(String principal_motivo) {
+		this.principal_motivo = principal_motivo;
+	}
+
+}
